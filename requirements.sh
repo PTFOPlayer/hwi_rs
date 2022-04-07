402e45e9e32339d4e3ced891bba284dc2fe70677
WhiskyAKM <35374730+PTFOPlayer@users.noreply.github.com>
2022-04-07 12:00:17 +0200

    Add files via upload

--- requirements.sh
+++ requirements.sh
@@ -1,2 +1,11 @@
+
+{
+    sudo apt install python3 python3-pip --noconfirm
+} || {
+    sudo pacman -S python3 python3-pip --noconfirm
+} || {
+    sudo dnf install python3 python3-pip 
+}
+
 pip install pyqt5 psutil py-cpuinfo
 
