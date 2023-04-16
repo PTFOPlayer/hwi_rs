f0b4dd777f8b51657751fe42d4574f10c0df146a
PTFOPlayer <patryk041103@gmail.com>
2023-04-16 11:14:46 +0200

    install.sh added

---
+++ install.sh
@@ -0,0 +1,6 @@
+cat ./install_deps/bin_part.* > ./install_deps/hwi_rs
+
+sudo cp ./install_deps/*.service /etc/systemd/system/
+sudo cp ./install_deps/msr_gen /usr/bin/
+sudo cp ./install_deps/hwi_rs /usr/bin
+sudo cp ./install_deps/hwi_rs.desktop /usr/share/applications/
