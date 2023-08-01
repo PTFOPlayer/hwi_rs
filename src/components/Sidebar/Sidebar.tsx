2b641068f4412ed69b5bd75f3b3c3b2a786b32a3
PTFOPlayer <patryk041103@gmail.com>
2023-08-01 20:07:48 +0200

    new binaries, fix in sidebar

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -4,7 +4,6 @@ export default function Sidebar() {
   return (
     <div className="sidebar">
       <a className="page" href="/"> main </a>
-      <a className="page" href="/#/nvoc"> nvoc </a>
     </div>
   )
 }
